df02e91b1afc5736ad6c142b56a7ba593c8232fd
Brandon Miller <BrandonMiller355@gmail.com>
2018-02-18 21:21:23 -0500

    Removed some of the stuff from that tutorial; Added my wall; Move my wall toward camera

---
+++ Assets/WallMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMovement : MonoBehaviour {
+    public Rigidbody rbWall;
+    public float incomingForce = -500f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rbWall.AddForce(0, 0, incomingForce * Time.deltaTime);
+    }
+}
