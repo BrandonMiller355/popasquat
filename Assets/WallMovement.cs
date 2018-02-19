eef2bf28321ee102b5cc05dc808ee0439c8a089c
Brandon Miller <Brandon.Miller@BlueSombrero.com>
2018-02-19 16:01:51 -0500

    Made wall speed constant; slowed it down a bunch

--- Assets/WallMovement.cs
+++ Assets/WallMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WallMovement : MonoBehaviour {
     public Rigidbody rbWall;
     public float incomingForce = -500f;
+    public float incomingSpeed = -2f;
 
     // Use this for initialization
     void Start()
@@ -15,6 +16,7 @@ public class WallMovement : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate()
     {
-        rbWall.AddForce(0, 0, incomingForce * Time.deltaTime);
+        //rbWall.AddForce(0, 0, incomingForce * Time.deltaTime);
+        rbWall.transform.Translate(Vector3.left * incomingSpeed * Time.deltaTime);
     }
 }
